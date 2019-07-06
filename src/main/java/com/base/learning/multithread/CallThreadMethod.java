e7d216b00bb333b90fa2c7bdd86286add3c285ff
flowerlake <cheeryg963@gmail.com>
2019-07-06 12:09:33 +0800

    invokeAll的使用

--- src/main/java/com/base/learning/multithread/CallThreadMethod.java
+++ src/main/java/com/base/learning/multithread/CallThreadMethod.java
@@ -1,11 +1,13 @@
 package com.base.learning.multithread;
 
-import java.util.Date;
+import static java.util.Arrays.asList;
+
+import java.util.*;
 import java.util.concurrent.*;
 
 import static com.sun.xml.internal.ws.client.ContentNegotiation.none;
 
-public class CallThreadMethod implements Callable<Object> {
+public class CallThreadMethod {
     /*
      * @program: java-learning
      * @description: 使用callable、线程池executor、future来创建可以返回值的线程
@@ -13,50 +15,60 @@ public class CallThreadMethod implements Callable<Object> {
      * @create: 2019-07-06 11:08
      * @version: 1.0
      */
-    private String taskName;
 
-    CallThreadMethod(String name) {
-        this.taskName = name;
-    }
+    static class CallThread implements Callable<Object> {
+        private String taskName;
 
-    @Override
-    public Object call() throws Exception {
-
-        System.out.println(Thread.currentThread().getName() + " start");
-
-        int sum = 0;
-        Date date1 = new Date();
-        for (int i = 0; i < 10; i++) {
-            sum = sum + i;
-            System.out.println(taskName + ":" + i);
+        CallThread(String name) {
+            this.taskName = name;
         }
-        Date date2 = new Date();
-        long time = date2.getTime() - date1.getTime();
 
-        System.out.println(Thread.currentThread().getName() + " end");
+        @Override
+        public Object call() throws Exception {
 
-        return sum;
+            System.out.println(Thread.currentThread().getName() + " start");
+
+            int sum = 0;
+            Date date1 = new Date();
+            for (int i = 0; i < 10; i++) {
+                sum = sum + i;
+                System.out.println(taskName + ":" + i);
+            }
+            Date date2 = new Date();
+            long time = date2.getTime() - date1.getTime();
+
+            System.out.println(Thread.currentThread().getName() + " end");
+
+            return sum;
+        }
     }
-}
-
-class CallMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int taskSize = 5;
+        int taskSize = 2;
         // 创建一个线程池
         ExecutorService executorServicePool = Executors.newFixedThreadPool(taskSize);
         // 创建具有返回值的任务
-        Callable callThreadMethod1 = new CallThreadMethod("AAa");
-        Callable callThreadMethod2 = new CallThreadMethod("BBA");
+        Callable callThreadMethod1 = new CallThread("AAa");
+        Callable callThreadMethod2 = new CallThread("BBA");
 
-        // 执行任务并获取 Future对象
+        // way-1:执行任务并获取 Future对象
         Future future1 = executorServicePool.submit(callThreadMethod1);
-        Future future2 = executorServicePool.submit(callThreadMethod2);
+        Future future2 = executorServicePool.submit(callThreadMethod1);
+
+        System.out.println("通过方式1获得的10的和为：" + future1.get().toString());
+        System.out.println("通过方式1获得的10的和为：" + future2.get().toString());
+
+        // =============================================
+        // way-2 通过invokeAll执行任务
+        List<Future<Object>> futureList = executorServicePool.invokeAll(asList(new CallThread("AAAA"), new CallThread("BBB")));
+
         // 关闭线程池
         executorServicePool.shutdown();
 
-        System.out.println("10的和为：" + future1.get().toString());
-        System.out.println("10的和为：" + future2.get().toString());
+        for (Future<Object> item : futureList) {
+            System.out.println("通过方式2获得的结果："+item.get());
+        }
+
     }
 
 }
